3db94ab7fb7ece1620843e311b718e6982cb967e
Oswaldo <oswald50valle@gmail.com>
2021-04-25 11:42:18 -0600

    Guia 5 Discusion de resultados completa

---
+++ app/src/main/java/com/example/guia5discusion_dsm/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.guia5discusion_dsm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
